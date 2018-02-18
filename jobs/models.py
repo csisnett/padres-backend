475c600576016d346b832bdcd0bb3c35c9240843
Carlos <sisnett2050@gmail.com>
2018-02-18 16:45:39 -0500

    Move models from padres to jobs

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+class JobMixin(models.Model):
+    """
+    Job Model
+    Defines the attributes of a job a person has/had
+    for a period of time.
+    """
+    title = models.CharField(max_length=130)
+    initial_date = models.DateField()
+    termination_date = models.DateField()
+    person = models.ManyToManyField('Person')
+    pay = models.IntegerField(null=True)
+    #benefits = Insert
+
+    class Meta:
+        abstract = True
+
+    #wasted_money = ...
+    #law_disorder = ...
+    #institution = ...
+    #wasted_money = ... models.ManyToManyField(Transaction)s
+
+
+class CongressJob(models.Model):
+    """
+    Model for the job of being a congress man/woman.
+    Defines the attributes, relations and methods of work in congress
+
+    a congress job is CongressJob()
+    """
+
+    uuid = models.UUIDField(
+    db_index=True,
+    default=uuid_lib.uuid4,
+    editable=False)
+    
+    description = models.TextField()
+    #absences
+
+
+class Institution(models.Model):
+    pass
