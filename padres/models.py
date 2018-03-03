35af4ce699c810f80d20c02b2bd6e76a4bdf6725
Carlos <sisnett2050@gmail.com>
2018-03-02 21:13:55 -0500

    make models more concise with mixins

--- padres/models.py
+++ padres/models.py
@@ -1,48 +1,31 @@
 from django.db import models
-import uuid as uuid_lib
+from utils.mixins import UUIDable, Descriptionable
+
+class Genderable(models.Model):
+    gender = models.ChoiceField()
 
 class Photo(models.Model):
     pass
 
-class Event(models.Model):
+class Event(Descriptionable, UUIDable, models.Model):
     title = models.CharField(max_length=140, blank=True, null=True)
-    description = models.TextField()
     date = models.DateField()
-    uuid = models.UUIDField(
-    db_index=True,
-    default=uuid_lib.uuid4,
-    editable=False)
 
 
 class Scandal(models.Model):
     pass
 
-class Promise(models.Model):
+class Promise(Descriptionable, UUIDable, models.Model):
     person = models.ManyToManyField('Person')
     title = models.CharField(max_length=140, blank=True, null=True)
-    uuid = models.UUIDField(
-    db_index=True,
-    default=uuid_lib.uuid4,
-    editable=False)
-    description = models.TextField()
 
-class Person(models.Model):
+class Person(Descriptionable, UUIDable, Genderable, models.Model):
     """
     Person Model
     Defines the attributes of a person
     """
     name = models.CharField(max_length=140)
     birthday = models.DateField()
-    gender = models.CharField(max_length=20)
     #picture = models.ImageField()
     #jobs = models.ForeignKey(Job, related_name='jobs',on_delete=models.PROTECT)
-    uuid = models.UUIDField(
-    db_index=True,
-    default=uuid_lib.uuid4,
-    editable=False)
-
-    def __repr__(self):
-        return self.name + ' is added.'
-        
-
-
+    owner = models.OneToOneField('transactions.Owner', on_delete='CASCADE')
