c08cde305a4fd44c21f1a2bbd0f9a79c6c864d72
Carlos <sisnett2050@gmail.com>
2018-05-07 16:55:18 -0500

    Change wsgi.py to current settings folder

--- config/wsgi.py
+++ config/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "padres.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "padres.config.settings.production")
 
 application = get_wsgi_application()
