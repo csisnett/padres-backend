71243c199a5f2e988a2704354167c803a1870df9
Carlos <sisnett2050@gmail.com>
2018-05-06 21:47:04 -0500

    Add production variables to production settings

---
+++ config/settings/production.py
@@ -0,0 +1,18 @@
+from .base import *
+from .settings_secret import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+REST_FRAMEWORK = {
+    
+    'DEFAULT_PERMISSION_CLASSES': [],
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json'
+}
+
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+X_FRAME_OPTIONS = 'DENY'
