20ec9195fcb2afdac9980ceaad6272d19cb1d27b
Carlos <sisnett2050@gmail.com>
2018-05-29 13:46:44 -0500

    Make StatementMixin, Statement, Believe and Source class, change Promise to use StatementMixin

--- src/padres/models.py
+++ src/padres/models.py
@@ -12,20 +12,39 @@ class Genderable(models.Model):
         abstract = True
 
 
+class StatementMixin(models.Model):
+
+    title = models.CharField(max_length=140)
+    exact_statement = models.TextField()
+    evidence = models.ManyToManyField('padres.Event', blank=True) #list of events that verify or not the statement
+    date = models.DateField(default='0001-01-01', blank=True)
+
+    class Meta:
+        abstract = True
+
 class Event(Descriptionable, UUIDable, models.Model):
     title = models.CharField(max_length=140, blank=True, null=True)
     date = models.DateField(default='0001-01-01', blank=True)
     people = models.ManyToManyField('Person', related_name='events')
-    resources = models.ManyToManyField('Resource')
+    sources = models.ManyToManyField('Source')
 
 class Scandal(UUIDable, Descriptionable, models.Model):
     events = models.ManyToManyField('Event')
     short_description= models.CharField(max_length=140, blank=True)
 
 
-class Promise(Descriptionable, UUIDable, models.Model):
-    people = models.ManyToManyField('padres.Person')
-    title = models.CharField(max_length=140, blank=False, null=True)
+class Promise(StatementMixin, Descriptionable, UUIDable, models.Model):
+    PROMISE_CHOICES = (
+    ('P', "Incumplida deliberadamente"),
+    ('N', 'Incumplida por inacción'),
+    ('H', 'Incumplida en parte'),
+    ('M', 'Cumplida en su mayoría'),
+    ('A', 'Cumplida completamente')
+    )
+
+    people = models.ManyToManyField('padres.Person', related_name='promises')
+    events = models.ManyToManyField('padres.Event')
+    category = models.CharField(max_length=3, choices=PROMISE_CHOICES, default='N')
 
 class Person(Descriptionable, UUIDable, Genderable, models.Model):
     """
@@ -33,16 +52,51 @@ class Person(Descriptionable, UUIDable, Genderable, models.Model):
     Defines the attributes of a person
     """
     name = models.CharField(max_length=140)
-    nickname = models.CharField(max_length=100)
-    birthday = models.DateField(blank=True)
+    nickname = models.CharField(max_length=100, blank=True)
+    birthday = models.DateField(blank=True, null=True)
     #picture = models.ImageField()
     #jobs = models.ForeignKey(Job, related_name='jobs',on_delete=models.PROTECT)
-    ownership = models.OneToOneField('transactions.Owner', on_delete='PROTECT', blank=True, null=True)
+    ownership = models.OneToOneField('transactions.Owner', on_delete='PROTECT', blank=True)
     short_description = models.CharField(max_length=140, blank=True)
-    family = models.ManyToManyField('Person')
-    scandals = models.ManyToManyField('Scandal', related_name='people')
+    family = models.ManyToManyField('Person', blank=True)
+    scandals = models.ManyToManyField('Scandal', related_name='people', blank=True)
 
+class PoliticalParty(Descriptionable, UUIDable, models.Model):
+    name = models.CharField(max_length=140)
+    people = models.ManyToManyField('Person', blank=True, related_name='party')
 
-class Resource(UUIDable, models.Model):
+class Statement(StatementMixin, Descriptionable, UUIDable, models.Model):
+    STATEMENT_CHOICES = (
+    ('LC', "Mentira Factual"),
+    ('LO', 'Mentira por Omisión'),
+    ('LF', 'Mentira por Influencia'),
+    ('T', "Es Cierto"),
+    ('UN', "No Verificado"),
+    ('O', "Opinión")
+
+    )
+    people = models.ManyToManyField('padres.Person', related_name='statements')
+    about_events = models.ManyToManyField('Event', blank=True) #if the statement is about an event
+    category = models.CharField(max_length=3, choices=STATEMENT_CHOICES, default='UN')
+
+class Believe(StatementMixin, Descriptionable, UUIDable, models.Model):
+    BELIEVE_CHOICES = (
+    ('H', "Mantiene esta creencia"),
+    ('C', 'Cambió  completamente'),
+    ('P', 'Cambió en parte'),
+    )
+
+    people = models.ManyToManyField('padres.Person', related_name='believes')
+    category = models.CharField(max_length=3, choices=BELIEVE_CHOICES, default='H')
+
+class Source(UUIDable, models.Model):
     url = models.URLField()
     published_date = models.DateField(default='0001-01-01', blank=True)
+
+class Contact(UUIDable, models.Model):
+    twitter = models.CharField(max_length=100, blank=True)
+    facebook = models.CharField(max_length=100, blank=True)
+    instagram = models.CharField(max_length=100, blank=True)
+    email_address = models.CharField(max_length=130, blank=True)
+    website = models.URLField(blank=True)
+    person = models.ForeignKey('padres.Person', related_name='contact_information')
