d921ce61006b4111cb4a7a6448c5f48ec11be2fd
Carlos <sisnett2050@gmail.com>
2018-07-09 13:00:38 -0500

    Change settings for elastic beanstalk

--- src/config/settings/production.py
+++ src/config/settings/production.py
@@ -33,11 +33,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', ''),
-        'USER': os.environ.get('DB_USER', ''),
-        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'NAME': os.environ.get('RDS_DB_NAME', ''),
+        'USER': os.environ.get('RDS_DB_USERNAME', ''),
+        'PASSWORD': os.environ.get('RDS_PASSWORD', ''),
         'HOST': os.environ.get('DB_HOST', ''),
-        'PORT': '',
+        'PORT': os.environ.get('RDS_PORT', ''),
     }
 }
 
